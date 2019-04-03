2af69e2247eb31dcdf568447db9eecff2ade36df
Michel El Habr <michel-haber@hotmail.com>
2019-04-04 00:48:31 +0200

    Halfway through Chap 06 exercices

--- 06-TypeFamilies/src/Exercises.hs
+++ 06-TypeFamilies/src/Exercises.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE GADTs         #-}
-{-# LANGUAGE TypeFamilies  #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Exercises where
 
 import Data.Kind (Constraint, Type)
@@ -22,8 +23,16 @@ data Nat = Z | S Nat
 -- | a. Use the @TypeOperators@ extension to rewrite the 'Add' family with the
 -- name '+':
 
+type family (+) (a :: Nat) (b :: Nat) :: Nat where
+  'Z     + b = b
+  ('S n) + m = 'S (n + m)
+
 -- | b. Write a type family '**' that multiplies two naturals using '(+)'. Which
 -- extension are you being told to enable? Why?
+
+type family (**) (a :: Nat) (b :: Nat) :: Nat where
+   'Z    ** b = 'Z
+   'S  n ** b = b + (n ** b)
 
 data SNat (value :: Nat) where
   SZ :: SNat 'Z
@@ -31,7 +40,9 @@ data SNat (value :: Nat) where
 
 -- | c. Write a function to add two 'SNat' values.
 
-
+add :: SNat n -> SNat m -> SNat (n + m)
+add  SZ    b = b
+add (SS a) b = SS (add a b)
 
 
 
@@ -44,15 +55,17 @@ data Vector (count :: Nat) (a :: Type) where
 -- | a. Write a function that appends two vectors together. What would the size
 -- of the result be?
 
--- append :: Vector m a -> Vector n a -> Vector ??? a
+append :: Vector m a -> Vector n a -> Vector (m + n) a
+append VNil ys = ys
+append (VCons x xs) ys = VCons x (append xs ys)
 
 -- | b. Write a 'flatMap' function that takes a @Vector n a@, and a function
 -- @a -> Vector m b@, and produces a list that is the concatenation of these
 -- results. This could end up being a deceptively big job.
 
--- flatMap :: Vector n a -> (a -> Vector m b) -> Vector ??? b
-flatMap = error "Implement me!"
-
+flatMap :: Vector m a -> (a -> Vector n b) -> Vector (m ** n) b
+flatMap  VNil        _ = VNil
+flatMap (VCons x xs) f = f x `append` flatMap xs f
 
 
 
@@ -61,11 +74,22 @@ flatMap = error "Implement me!"
 
 -- | a. More boolean fun! Write the type-level @&&@ function for booleans.
 
+type family (&&) (a :: Bool) (b :: Bool) :: Bool where
+  'True  && b = b
+  'False && _ = 'False
+
 -- | b. Write the type-level @||@ function for booleans.
+
+type family (||) (a :: Bool) (b :: Bool) :: Bool where
+  'True  || _ = 'True
+  'False || b = b
 
 -- | c. Write an 'All' function that returns @'True@ if all the values in a
 -- type-level list of boleans are @'True@.
 
+type family All (ls :: [Bool]) :: Bool where
+  All '[]       = 'True
+  All (x ': xs) = x && All xs
 
 
 
@@ -75,22 +99,45 @@ flatMap = error "Implement me!"
 -- | a. Nat fun! Write a type-level 'compare' function using the promoted
 -- 'Ordering' type.
 
+type family Compare (a :: Nat) (b :: Nat) :: Ordering where
+  Compare 'Z 'Z = 'EQ
+  Compare  x 'Z = 'GT
+  Compare 'Z  y = 'LT
+  Compare  ('S x) ('S y) = Compare x y
+
 -- | b. Write a 'Max' family to get the maximum of two natural numbers.
+
+type family Max (a :: Nat) (b :: Nat) :: Nat where
+  Max a b = Choose (Compare a b) a b
+
+type family Choose (o :: Ordering) (a :: Nat) (b :: Nat) :: Nat where
+  Choose 'GT x _ = x
+  Choose  _  _ y = y
 
 -- | c. Write a family to get the maximum natural in a list.
 
-
+type family MaxList (xs :: [Nat]) :: Nat where
+  MaxList '[] = 'Z
+  MaxList (x ': xs) = Max x (MaxList xs)
 
 
 
 {- FIVE -}
 
+-- This tree is meant as a binary search tree without duplication
+-- I inferred that after comparing my solution with the provided solution
 data Tree = Empty | Node Tree Nat Tree
 
 -- | Write a type family to insert a promoted 'Nat' into a promoted 'Tree'.
 
+type family Insert (t :: Tree) (n :: Nat) :: Tree where
+  Insert 'Empty        n = 'Node 'Empty n 'Empty
+  Insert ('Node l c r) n = Branch (Compare n c) ('Node l c r) n
 
-
+type family Branch (o :: Ordering) (t :: Tree) (n :: Nat) :: Tree where
+  Branch 'EQ t             _ = t
+  Branch 'GT ('Node l c r) n = 'Node l c (Insert r n)
+  Branch 'LT ('Node l c r) n = 'Node (Insert l n) c r
 
 
 {- SIX -}
